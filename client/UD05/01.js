e40274a429693865cdae667640b0cc1f9d46944c
Marcos <monorata2@hotmail.com>
2019-11-11 21:52:13 +0100

    JS array example

---
+++ client/UD05/01.js
@@ -0,0 +1,5 @@
+const provincias = new Map();
+provincias.set(1,"new").set(20,'Madrid').set(20,'Madrid').set(20,'Madrid');
+provincias.forEach(function(valor,clave)) {
+    console.log('clave:'$clave);
+}
