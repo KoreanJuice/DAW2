4c8ce3aded242bfaa9db2ee4b8f7ec01e00e08e9
Marcos <monorata2@hotmail.com>
2019-11-19 20:12:59 +0100

    server steps

---
+++ server/task/vuelos.php
@@ -0,0 +1,10 @@
+<?php
+  $vuelo = array("idvuelo", "salida", "destino", "hora", "fila", 1, 2, 3, 4, 5, 6 =>
+  [
+    ('AK127', 'Madrid', 'Londres', '00:25', 1, 0, 1, 1, 0, 0, 1),
+    ('AK127', 'Madrid', 'Londres', '00:25', 1, 0, 1, 1, 0, 0, 1)
+  ];
+var_dump($vuelo);
+/*
+
+*/
