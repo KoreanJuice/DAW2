46e0a9883291587a55c44926583b9eb8b19e53a7
Marcos <monorata2@hotmail.com>
2019-11-22 20:10:02 +0100

    client try

--- interfaces/PP0401/js/main.js
+++ interfaces/PP0401/js/main.js
@@ -1,14 +1,31 @@
+const menu_bar = document.getElementsByClassName('.menu_bar')[0];
+
+const toggleMenu = () => {
+	const menu = document.getElementsByTagName('nav')[0];
+	const menuOpen = menu.classList.contains('active');
+
+	if (menuOpen) {
+			//Close
+			menu.classList.remove('active');
+	} else {
+			// Open
+			menu.classList.add('active');
+	}
+};
+
+menu_bar.addEventListener('click', () => {
+	toggleMenu();
+});
+//////////////////////////////////////////////////////
 // const menu_bar = document.querySelector('.menu_bar');
 //
 // const toggleMenu = () => {
-//
 // 	const menu = document.querySelector('nav');
 // 	const menuOpen = menu.classList.contains('active');
 //
 // 	if (menuOpen) {
 // 			//Close
 // 			menu.classList.remove('active');
-//
 // 	} else {
 // 			// Open
 // 			menu.classList.add('active');
@@ -18,13 +35,14 @@
 // menu_bar.addEventListener('click', () => {
 // 	toggleMenu();
 // });
-const main = () => {
-	$('.menu_bar').click(function(){
-		$('nav').slideToggle();
-	});
-
-	$('.submenu').click(function(){
-		$(this).children('.children').slideToggle();
-	});
-}
-$(document).ready(main);
+//////////////////////////////////////////////////////
+// const main = () => {
+// 	$('.menu_bar').click(function(){
+// 		$('nav').slideToggle();
+// 	});
+//
+// 	$('.submenu').click(function(){
+// 		$(this).children('.children').slideToggle();
+// 	});
+// }
+// $(document).ready(main);
