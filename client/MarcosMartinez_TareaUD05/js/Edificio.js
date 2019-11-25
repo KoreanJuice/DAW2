e14c8c79e6cd71c5683d30894866b1cd1a9fc142
Marcos <monorata2@hotmail.com>
2019-11-25 21:54:01 +0100

    javascript task UD05

---
+++ client/MarcosMartinez_TareaUD05/js/Edificio.js
@@ -0,0 +1,46 @@
+
+
+const Edificio = (calle = 'null', numero = '0', cPostal = '0', plantas) => {
+    let calle = calle;
+    let numero = numero;
+    let cPostal = cPostal;
+    let plantas = plantas;
+    /* add to html => 
+        construido nuevo edificio en calle: {calle}, nº: {numero}, CP: {cPostal}.
+    */
+}
+
+const agregarPlantasYPuertas = (plantas, puertas) => {
+
+}
+
+const modificarNumero = (numero) => {
+    this.numero = numero;
+}
+
+const modificarCodigoPostal = (codigo) => {
+    this.cPostal = codigo;
+}
+
+const agregarPropietario = (nombre, planta, puerta) => {
+    /* add to html => 
+        {nombre} es ahora el propietario de la puerta {puerta} de la planta {planta}.
+        (datos cualesquiera)
+    */
+}
+
+const imprimeCalle = () => {
+    return this.calle;
+}
+
+const imprimeNumero = () => {
+    return this.numero;
+}
+
+const imprimeCodigoPostal = () => {
+    return this.cPostal;
+}
+
+const imprimePlantas = () => {
+    return this.plantas;
+}
---
+++ client/MarcosMartinez_TareaUD05/js/puerta.js
@@ -0,0 +1,5 @@
+
+
+const Puerta = (propietario = 'null') => {
+    let propietario = propietario;
+}
