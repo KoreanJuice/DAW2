c7e4a07f8196f1cff1bbae3f4663dd1d048f3709
Marcos <monorata2@hotmail.com>
2019-11-25 19:32:17 +0100

    tab fix

--- interfaces/PP0401/js/main.js
+++ interfaces/PP0401/js/main.js
@@ -5,27 +5,27 @@ let navOpen = false;
 function main () {
     // show/hide menus
     $('.menu_bar').click(function(){
-		if (!navOpen) {
-			$('nav').css({
-                "display": "inline-block",
-                "width": "100%"
-            });
-			navOpen = true;
-		} else {
-            $('nav').css("display", "none");
-			navOpen = false;
-		}
-	});
+      if (!navOpen) {
+        $('nav').css({
+          "display": "inline-block",
+          "width": "100%"
+        });
+        navOpen = true;
+      } else {
+        $('nav').css("display", "none");
+        navOpen = false;
+      }
+    });
     // idem submenus
     $('.submenu').click(function(){
-        $(this).children('.children').slideToggle();
+      $(this).children('.children').slideToggle();
     });
 
     // fix tablet to desktop nav => display: none
     $(document).ready(function(){
-        $(window).resize(function(){
-            if (window.innerWidth >= 1024) {$('nav').css("display", "block");}
-            else {$('nav').css("display", "none");}
-        });
+      $(window).resize(function(){
+        if (window.innerWidth >= 1024) {$('nav').css("display", "block");}
+        else {$('nav').css("display", "none");}
+      });
     });
 }
