83e78c9a297e5dc99a1f6bc1e057405ff3acbba5
Marcos <monorata2@hotmail.com>
2020-01-17 21:07:24 +0100

    CursosDisenioWeb init

---
+++ server/task/CursosDisenioWeb/app/plantillas/cartelera.php
@@ -0,0 +1,7 @@
+<?php ob_start() ?>
+<h2>FUNCIONES DE TEATRO - PRONTO</h2>
+<a href="index.php?ctl=formulario">link a formulario de venta</a>
+<?= $owo ?>
+<?php $contenido = ob_get_clean() ?>
+
+<?php require_once 'base.php' ?>
